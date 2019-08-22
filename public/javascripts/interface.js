2ee8469badc2adea596837e208f4d1dab5f45ae3
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-08-22 20:10:38 +0100

    Added weather api

--- public/javascripts/interface.js
+++ public/javascripts/interface.js
@@ -2,6 +2,25 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
+  $.get(
+    "http://api.openweathermap.org/data/2.5/weather?q=london&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric",
+    function(data) {
+      $("#current-temperature").text(data.main.temp);
+    }
+  );
+
+  $("#current-city").change(function() {
+    var city = $("#current-city").val();
+    $.get(
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric",
+      function(data) {
+        $("#current-temperature").text(data.main.temp);
+      }
+    );
+  });
+
   $("#temperature-up").click(function(event) {
     if (thermostat.showTemperature() === thermostat.showMaxTemperature()) {
       return;
